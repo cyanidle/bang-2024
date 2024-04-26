14e6e8289f7f908c12b30dd019a10d8e09820206
Alexej Doronin <lyosha.doronin@gmail.com>
2024-04-26 18:33:51 +0200

    ((((

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 #!/usr/bin/env python
+import atexit
 from threading import Thread
 from time import sleep
-
 from bang import *
-from bang.gen import MsgConfigMotor, MsgConfigPinout
 
 def Task(func):
     Thread(target=func, daemon=True).start()
@@ -52,6 +51,8 @@ def handle_odom(msg):
 
 @bang.arduino.on(MsgEcho)
 def handle_echo(echo): print(f"<== {echo}")
+@bang.arduino.on(MsgTest)
+def handle_test(msg): print(f"Test: {msg}")
 
 def move(x, y, z): bang.arduino.send(MsgMove(x, y, z))
 def stop(): move(0, 0, 0)
@@ -66,6 +67,38 @@ for params, pinout in zip(motors, pinouts):
     sleep(0.3)
     bang.arduino.send(params)
 
-while True:
+def shutdown():
+    global bang
+    stop()
+    del bang
+    sleep(0.3)
+
+atexit.register(shutdown)
+
+START_PIN = 48
+START_ROUND = False
+
+@bang.arduino.on(MsgReadPin)
+def handle_pin(msg: MsgReadPin): 
+    global START_ROUND
+    print(f"Got pin: {msg}")
+    if msg.value and msg.pin == START_PIN:
+        print("Starting round")
+        START_ROUND = True
+
+
+def start():
     sleep(0.5)
-    forward(1)
+    while True: 
+        forward(1000)
+        sleep(3)
+        turn(1000)
+        sleep(3)
+        turn(-1000)
+
+while True:
+    sleep(0.1)
+    bang.arduino.send(MsgReadPin(START_PIN, 0, pullup=1))
+    if START_ROUND:
+        start()
+        break
